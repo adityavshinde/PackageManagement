3a3fdd70f910684c2ca580f804bba5ccdbfe9e15
Aditya Shinde <9shindeaditya@gmail.com>
2025-05-19 00:10:00 +0000

    logger added

--- src/main/java/com/manage/product_management/controller/AdminController.java
+++ src/main/java/com/manage/product_management/controller/AdminController.java
@@ -1,5 +1,9 @@
 package com.manage.product_management.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 import com.manage.product_management.model.LoginTable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,6 +17,9 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class AdminController {
+
+	private static final Logger logger = LogManager.getLogger(AdminController.class);
+
     @Autowired
     private adminrepository adminrepo;
     //private ResourceNotFoundException except;
@@ -26,6 +33,7 @@ public class AdminController {
     @GetMapping("/showadmins")
     public List<LoginTable> getAllAdmins()
     {
+        logger.info("Getting all the admins in a list and sending to the frontend");
         return adminrepo.findAll();
     }
 
@@ -37,23 +45,30 @@ public class AdminController {
         if(user == null)
         {
             System.out.println("User does not exist");
+            logger.error("User does not exist");
 //            return except.("User DoesNot exist");
              //return new ResponseEntity<String>("Wrong ID or Password", HttpStatus.NOT_FOUND);
-            //return user;
+            return ResponseEntity.badRequest().body(null);
         }
+        logger.info("Valid user..." + user);
         return ResponseEntity.ok().body(user);
     }
     @GetMapping("/login")
     public ResponseEntity<LoginTable> loginuserget(@RequestBody LoginTable user)
     {
+//     	String email = user.getEmailId();
+//        String pass = user.getPassword();
+//        System.out.println("email"+ email+" "+pass);
         user = adminrepo.findByUsernameAndPassword(user.getUsername(), user.getPassword());
         if(user == null)
         {
             System.out.println("User does not exist");
+            logger.error("User does not exist");
             //return except.("User DoesNot exist");
 //            return ResponseEntity<String>("Wrong ID or Password", HttpStatus.NOT_FOUND);
-            //return user;
+            return null;
         }
+        logger.info("New user registered..." + user);
         return ResponseEntity.ok().body(user);
     }
 
@@ -63,10 +78,14 @@ public class AdminController {
     @PostMapping("/register")
     public LoginTable registeruser(@RequestBody LoginTable user)
     {
-
-        System.out.println(user.toString());
-        adminrepo.saveAndFlush(user);
-        System.out.println(user.toString());
+	if(user==null)
+	{
+		logger.error("null input...");
+	}
+	logger.info("New user registered..." + user);
+        //System.out.println(user.toString());
+        adminrepo.save(user);
+        //System.out.println(user.toString());
         return user;
     }
 
--- src/main/java/com/manage/product_management/controller/RecordController.java
+++ src/main/java/com/manage/product_management/controller/RecordController.java
@@ -5,12 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.manage.product_management.model.RecordTable;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class RecordController {
+
+    private static final Logger logger = LogManager.getLogger(AdminController.class);
+
     @Autowired
     private recordrepository recordrep;
 
@@ -26,9 +31,15 @@ public class RecordController {
 
         RecordTable record = recordrep.findByTrackId(track_id);
 
+        if(record==null)
+        {
+            logger.error("Record does not exist");
+            return ResponseEntity.badRequest().body(null);
+        }
+
         String cmp = record.getCompName();
         System.out.println(cmp);
-
+        logger.info("Record Found");
         return ResponseEntity.ok().body(record);
     }
     @DeleteMapping("/deleterecord/{track_id}")
@@ -37,23 +48,25 @@ public class RecordController {
         RecordTable record=recordrep.findByTrackId(track_id);
         if(record==null)
         {
+            logger.error("Record does not exist");
             return false;
         }
         else
         {
+            logger.info("Record Found and successfully deleted");
             recordrep.delete(record);
             return true;
         }
-
     }
 
     @PostMapping("/addrecord")
     public RecordTable addrecord(@RequestBody RecordTable record)
     {
-        recordrep.saveAndFlush(record);
+        logger.info("Record added to the database successfully "+ record);
+        recordrep.save(record);
         return record;
     }
-//
+
 //    @PostMapping("/updaterecord")
 //    public RecordTable updateRecord(@RequestBody RecordTable record)
 //    {
@@ -63,6 +76,7 @@ public class RecordController {
     @GetMapping("/viewrecords")
     public List<RecordTable> getAllAdmins()
     {
+        logger.info("Getting the list and sending to the frontend");
         return recordrep.findAll();
     }
 
